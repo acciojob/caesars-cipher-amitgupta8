f3bd2810f17f2559f492e7a6d680f78a2c0b9e66
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-15 00:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-// Your Script here.
-console.log(rot13('EBG13 rknzcyr.')); // "ROT13 example."
-
+function rot13(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    // Get the character code of the current character
+    const charCode = str.charCodeAt(i);
+    let decodedCharCode;
+    if (charCode >= 65 && charCode <= 90) {
+      // Shift the character code by 13
+      decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
+    } else {
+      // Keep non-alphabetic characters as-is
+      decodedCharCode = charCode;
+    }
+    // Convert the character code back to a character and add it to the result string
+    result += String.fromCharCode(decodedCharCode);
+  }
+  return result;
+}
