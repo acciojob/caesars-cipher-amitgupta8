b51da4e91af1cf11fcdc7e97ac7d941e392f921a
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-27 15:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function rot13(str) {
-  let result = "";
+  let decodedStr = "";
+
   for (let i = 0; i < str.length; i++) {
-    // Get the character code of the current character
-    const charCode = str.charCodeAt(i);
-    let decodedCharCode;
+    let charCode = str.charCodeAt(i);
+
     if (charCode >= 65 && charCode <= 90) {
-      // Shift the character code by 13
-      decodedCharCode = ((charCode - 65 + 13) % 26) + 65;
-    } else {
-      // Keep non-alphabetic characters as-is
-      decodedCharCode = charCode;
+      charCode = ((charCode - 65 + 13) % 26) + 65;
     }
-    // Convert the character code back to a character and add it to the result string
-    result += String.fromCharCode(decodedCharCode);
+
+    decodedStr += String.fromCharCode(charCode);
   }
-  return result;
+
+  return decodedStr;
 }
